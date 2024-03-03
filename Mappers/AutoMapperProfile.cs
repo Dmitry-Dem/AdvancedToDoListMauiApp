2ea2caca7b2592160714c38f4dab829a360319ff
Dmitry-Dem <demenchuk.dmitry03@gmail.com>
2024-03-03 18:41:29 +0200

    Added AutoMapper.

---
+++ Mappers/AutoMapperProfile.cs
@@ -0,0 +1,14 @@
+﻿using AdvancedToDoListMauiApp.Models;
+using AutoMapper;
+
+namespace AdvancedToDoListMauiApp.Mappers;
+public class AutoMapperProfile : Profile
+{
+    public AutoMapperProfile()
+    {
+        CreateMap<Punishment, PunishmentChanges>()
+            .ForMember(x => x.Name, opt => opt.MapFrom(src => src.Description))
+            .ForMember(x => x.PunishmentId, opt => opt.MapFrom(src => src.Id));
+    }
+}
+
